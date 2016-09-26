28e0b50e99b2f1f5ef4d8ad7f9ca528a11acea70
Zejnadze Zi <aziz.zamaliev@yandex.ru>
2016-09-26 23:00:27 +0300

    New index file

--- pesok/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$mysqli = new mysqli('localhost', 'moko', 'moko', 'mojka'); 
-
-	if (mysqli_connect_errno()) { 
-	   printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error()); 
-	   exit; 
-	}
-	$mysqli->set_charset("utf8") 
-?>
